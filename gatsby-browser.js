d754597976cea642dd710122835abc0e9afb7ce5
Eugene Draitsev <Eugene.Draitsev@ebuilder.com>
2020-01-14 16:41:18 +0100

    Init commit

---
+++ gatsby-browser.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: slategray;
+  }
+`
+
+// eslint-disable-next-line react/prop-types
+export const wrapRootElement = ({ element }) => (
+  // eslint-disable-next-line react/jsx-filename-extension
+  <>
+    <GlobalStyle />
+    {element}
+  </>
+)
